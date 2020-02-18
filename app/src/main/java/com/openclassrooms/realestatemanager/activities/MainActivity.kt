b1f54ecc7713b4c33021a8f662dbffe8bb50df15
jpz <jeanpierrezingraff@gmail.com>
2020-02-18 23:54:03 +0100

    Add Fragment dependency and Google Material (which was cancel by inadvertence). Configure RecyclerView in ListFragment and add the fragment with a toolbar in MainActivity.

--- app/src/main/java/com/openclassrooms/realestatemanager/activities/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activities/MainActivity.kt
@@ -2,12 +2,34 @@ package com.openclassrooms.realestatemanager.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.fragments.ListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    // For design
+    private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureToolbar()
+
+        val listFragment = ListFragment()
+        supportFragmentManager.beginTransaction()
+                .add(R.id.activity_main_fragment_container_view, listFragment)
+                .commit()
     }
+
+    // Private methods to configure design
+
+    private fun configureToolbar() {
+        // Get the toolbar view inside the activity layout
+        toolbar = findViewById(R.id.toolbar)
+        // Set the Toolbar
+        setSupportActionBar(toolbar)
+    }
+
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/fragments/ListFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/fragments/ListFragment.kt
@@ -2,23 +2,68 @@ package com.openclassrooms.realestatemanager.fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.adapters.ItemAdapter
+import com.openclassrooms.realestatemanager.models.Item
+
 
 /**
  * A simple [Fragment] subclass.
  */
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), ItemAdapter.Listener {
+
+    private var recyclerView: RecyclerView? = null
+    private var itemAdapter: ItemAdapter? = null
+
+    private var item: Item = Item(0, "type", 100, 10, 2,
+            "description", listOf("photo"), "address", "district",
+            listOf("poi"), "status", "01/01/01", "02/02/02", "bobby")
+
+    private var itemList: MutableList<Item> = mutableListOf()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val fragmentView: View = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Get RecyclerView from layout and serialise it
+        recyclerView = fragmentView.findViewById(R.id.fragment_list_recycler_view)
+
+        configureRecyclerView()
+        itemList.add(0, item)
+        updateUI(itemList)
+
+        return view
     }
 
+    override fun onClickItem(position: Int) {
+    }
 
+    // Configure RecyclerViews, Adapters & LayoutManager
+
+    private fun configureRecyclerView() {
+        // Reset list
+        //itemList = null
+        //itemList.add(0, item)
+        // Create the adapter by passing the list of articles
+        itemAdapter = ItemAdapter(itemList, Glide.with(this), this)
+        // Attach the adapter to the recyclerView to populate items
+        recyclerView?.adapter = itemAdapter
+        // Set layout manager to position the items
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+    }
+
+    private fun updateUI(updateList: List<Item>) {
+        // Add the list from the request and notify the adapter
+        itemList.addAll(updateList)
+        itemAdapter?.notifyDataSetChanged()
+    }
 }
